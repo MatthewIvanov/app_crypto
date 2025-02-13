be648e53e3c127a690f849162bd06d21a45d83f6
Matthew <108363565+MatthewIvanov@users.noreply.github.com>
2025-02-13 21:39:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Trading App"
+)
+fake_users=[
+    {"id":1,"role":"admin","name":"Bob"},
+    {"id":2,"role":"investor","name":"John"},
+    {"id":3,"role":"trader","name":"Matthew"},
+]
+@app.get("/users/{user_id}")
+def get_user(user_id:int):
+    return [user for user in fake_users if user.get("id")==user_id]
